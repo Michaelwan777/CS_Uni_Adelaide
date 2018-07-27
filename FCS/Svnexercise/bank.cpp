f4708afe2b0276c1d4b2a9350f80bc26c9530fa2
Sichen Wan <a1698799@student.adelaide.edu.au>
2018-07-28 00:04:30 +0800

    d

--- FCS/Svnexercise/bank.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  int money,i;
-  cout<<"how much money do you want to withdraw "<<endl;
-  cin>>money;
-  
-}
--- FCS/Workshop 11/1.1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
- int *t = new int;
- cout<<"*t  "<<*t<<"  "<<  sizeof(*t)<<endl;
- cout<<"t  "<<t<<"  "<< sizeof(t)<<endl;
- return 0;
-}
